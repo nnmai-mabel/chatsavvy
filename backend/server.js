85d5a3f364f0075ed0bade8b41c74195c52ca849
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-01 09:15:32 +1030

    npm install nodemon --save-dev

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 const express = require("express")
+
+// Create express server
+const app = express();
+
+// Test route
+app.get("/", (req, res) => {
+    // root route http://localhost:5001/
+    res.send("Hello World");
+})
+app.listen(5001, () => console.log("server running on port 5001"));
