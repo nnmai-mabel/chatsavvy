f571742cd93b962f74f5824f9e2b5b77f3063794
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-01 13:42:33 +1030

    hash password using bcrypt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,23 +1,32 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-    try{
-        const {fullName, username, password, confirmPassword, gender} = req.body;
-        if(password !== confirmPassword){
-            return res.status(400).json({error: "Passwords don't match"})
+    try {
+
+        // Get input from user
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+
+        // Check if password doesn't match
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords don't match" })
         }
 
         // Check if this user exists in the database with this username
-        const user = await User.findOne({username})
+        const user = await User.findOne({ username })
 
         // If user exists
-        if(user){
-            return res.status(400).json({error: "Username already exists"})
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" })
         }
 
+        // If user not exist, create hashed password and create user
         // HASH Password here
+        const salt = await bcrypt.genSalt(10); // higher is more secure but slower as well
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         // Avatar based on username
+        // https://avatar-placeholder.iran.liara.run/
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}}`
 
@@ -25,25 +34,30 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
 
-        // Save new user data to MongoDB
-        await newUser.save();
+        // If there is new user
+        if (newUser) {
+            // Save new user data to MongoDB
+            await newUser.save();
 
-        // 201 means created
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+            // 201 means created
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({error: "Invalid user data"});
+        }
 
-    } catch (error){
+    } catch (error) {
         console.log("Error in signup controller", error.message);
-        res.status(500).json({error:"Internal Server Error"});
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
