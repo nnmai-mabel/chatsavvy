df34c4934b69c610ad84544a944aaaf5eece9961
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-03 21:27:00 +1030

    get conversations for all users

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,28 @@
+import { useEffect } from "react"
+import useConversation from "../../zustand/useConversation"
 import MessageInput from "./MessageInput"
 import Messages from "./Messages"
 import {TiMessages} from "react-icons/ti"
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const {selectedConversation, setSelectedConversation} = useConversation()
+
+  // Unmount to make the state disappear after logging in back
+  useEffect(() => {
+	return () => setSelectedConversation(null)
+  }, [setSelectedConversation])
   return (
 
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
 
         <>
           {/* Header */}
           <div className="bg-slate-200 px-4 py-2 mb-2">
-            <span className="label-text">To:</span> <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="label-text">To: </span> 
+			<span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
           </div>
 
           <Messages />
