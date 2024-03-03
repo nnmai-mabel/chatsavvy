af62ac6efa7826b5a5be90be851edf2e3f982ea9
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-03 17:51:20 +1030

    authenticate user and navigate to appropriate pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import {Toaster} from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 
 function App() {
+	const {authUser} = useAuthContext()
 
 	return (
 		<div className='p-4 h-screen flex items-center justify-center'>
@@ -13,9 +15,9 @@ function App() {
 			{/* <SignUp /> */}
 			{/* <Home /> */}
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/signup' element={<SignUp />} />
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to="/" /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to="/" /> : <SignUp />} />
 			</Routes>
 			<Toaster />
 		</div>
