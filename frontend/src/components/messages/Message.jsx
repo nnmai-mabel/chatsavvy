8baed8fd3767cec66a03772e29a27c2fc5f217ef
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-03 11:52:17 +1030

    message container

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='chat chat-end'>
+        <div className='chat-image avatar'>
+            <div className='w-10 rounded-full'>
+                <img 
+                    alt='Tailwind css chat bubble'
+                    src={
+                        "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    }
+                />
+            </div>
+        </div>
+        <div className={`chat-bubble text-white bg-blue-900`}>
+            Hi What's up?
+        </div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>6:06</div>
+    </div>
+  )
+}
+
+export default Message
