f68420bd26dda08fd83893d1ccf4659953d1d0e4
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-04 14:02:14 +1030

    add notification sound for new messages

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import {useSocketContext} from '../context/SocketContext'
 import useConversation from '../zustand/useConversation'
+import notificationSound from "../assets/sounds/notification.mp3"
 
 const useListenMessages = () => {
   const {socket} = useSocketContext()
@@ -10,10 +11,15 @@ const useListenMessages = () => {
     socket?.on("newMessage", (newMessage) => {
         // New message shake
         newMessage.shouldShake = true
+        const sound = new Audio(notificationSound)
+        sound.play()
 
         // Add all previous messages, new message at the end
         setMessages([...messages, newMessage])
     })
+
+    // If don't add this, a lot of events will be listened on socket, whenever send a new message,
+    // will here notification sound multiple times
     return () => socket.off("newMessage")
   }, [socket, setMessages, messages])
 }
