3a84a49adb2c19d2202781d4e69723a25ce3f029
Mai (Mabel) Nguyen <137015107+nnmai-mabel@users.noreply.github.com>
2024-04-18 08:12:39 +0930

    Format source code link

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -6,13 +6,13 @@ const LogoutButton = () => {
   const {loading, logout} = useLogout()
 
   return (
-    <div className='mt-auto'>
+    <div className='mt-auto flex items-center'>
       {!loading ? (
         <RiLogoutCircleLine onClick={logout} className='w-6 h-6 text-white cursor-pointer' />
       ) : (
         <span className='loading loading-spinner'></span>
       )}
-      <Link to="https://github.com/nnmai-mabel/chatsavvy">Source code</Link> 
+      <Link to="https://github.com/nnmai-mabel/chatsavvy" className="text-white ml-2" target="_blank" rel="noopener noreferrer">Source code</Link> 
     </div>
   )
 }
